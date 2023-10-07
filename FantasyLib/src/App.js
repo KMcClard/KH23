83e80c8074daa1b964cfec189832e82c7946cc55
prdevoney <prdevo@gmail.com>
2023-10-07 14:33:28 -0400

    merge fix

--- FantasyLib/src/App.js
+++ FantasyLib/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { component } from 'react';
 import './index.css';
 import { useEffect, useState } from "react";
@@ -44,39 +43,6 @@ function App() {
           return <img src={pic} />;
         })}
     </main>
-=======
-import React, { useState } from 'react';
-import './index.css'; // Import your CSS file
-
-function App() {
-  const [image, setImage] = useState(null);
-
-  function handleImage(e) {
-    setImage(e.target.files[0]);
-  }
-
-
-  // May need this to send request to backend: 
-  // const response = await fetch ('http://localhost:8000/completions', options)
-
-  function handleSubmit() {
-    if (image) {
-      // You can perform actions with the selected image here
-      console.log('Selected image:', image);
-    } else {
-      console.log('No image selected');
-    }
-  }
-
-  return (
-    <div className="person">
-      <label>
-        Upload Image
-        <input type="file" name="file" onChange={handleImage} />
-      </label>
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
->>>>>>> b4022f66d0b01c20161473976d761fdd571aac69
   );
 }
 

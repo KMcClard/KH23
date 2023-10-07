5732df8329be5c0442b6d2d1e49034cb5cb7c95b
s1nno <haydensandler1@gmail.com>
2023-10-07 14:28:18 -0400

    image upload update

--- FantasyLib/src/App.js
+++ FantasyLib/src/App.js
@@ -1,24 +1,95 @@
 import React, { component } from 'react';
-import {useState} from 'react'
 import './index.css';
-
+import { useEffect, useState } from "react";
 
 function App() {
-  const[image, setImage] = useState('')
-  function handleImage(e){
-    console.log(e.targer.files[0])
-    setImage(e.target.files[0])
-  }
+  const [files, setFiles] = useState();
+  const [previews, setPreviews] = useState();
+
+  // rendering previews
+  useEffect(() => {
+    if (!files) return;
+    let tmp = [];
+    for (let i = 0; i < files.length; i++) {
+      tmp.push(URL.createObjectURL(files[i]));
+    }
+    const objectUrls = tmp;
+    setPreviews(objectUrls);
+
+    // free memory
+    for (let i = 0; i < objectUrls.length; i++) {
+      return () => {
+        URL.revokeObjectURL(objectUrls[i]);
+      };
+    }
+  }, [files]);
 
   return (
-
-        <div className="person">
-
-          <input type="file" name='file' onChange={handleImage}/>
-          <button>submit</button>
-        </div>
-    
-  )
+    <main className="container">
+      <br />
+      <h3>Fantasy Lens</h3>
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png"
+        multiple
+        onChange={(e) => {
+          if (e.target.files && e.target.files.length > 0) {
+            setFiles(e.target.files);
+          }
+        }}
+      />
+      {previews &&
+        previews.map((pic) => {
+          return <img src={pic} />;
+        })}
+    </main>
+  );
 }
 
 export default App;
+// // import { axios } from 'axios'
+
+
+// function App() {
+//   const[image, setImage] = useState('')
+
+//   function handleImage(e){
+//     setImage(e.target.files[0])
+//     return e.target.files[0];
+//   }
+//   const imageUrl = URL.createObjectURL(image)
+
+//   // Revoke the URL when the component unmounts
+//   useEffect(() => {
+//     return () => {
+//       URL.revokeObjectURL(imageUrl)
+//     }
+//   }, [imageUrl])
+
+//   // function handleApi(){
+//   //   const formData = new FormData()
+//   //   formData.append('image', image)
+//   //   axios.post('url', formData).then((res) => {
+//   //     console.log(res)
+//   //   })
+//   // }
+
+//   return (
+
+//         <div className="person">
+
+//           <input 
+//             type="file" 
+//             name='file' 
+//             accept="image/jpg, image/jpeg, image/png"
+//             onChange={handleImage}/>
+//           <img src={imageUrl} alt="User Image" />
+          
+//           {/* <button onClick={handleApi}>submit</button> */}
+//         </div>
+        
+    
+//   )
+// }
+
+// export default App;

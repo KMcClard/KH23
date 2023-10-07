2e96a06530f05115958dee7a9cb4eeba8044b8b5
prdevoney <prdevo@gmail.com>
2023-10-07 14:31:47 -0400

    server

---
+++ FantasyLib/server.js
@@ -0,0 +1,14 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const API_KEY = process.env.API_KEY
+
+app.post('/completions', async (req, res) => {
+    const { image } = req.body
+    console.log(image)
+    

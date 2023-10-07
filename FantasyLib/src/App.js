adeb9cebd601d3be3881f2522cc4a049250cc1d7
s1nno <haydensandler1@gmail.com>
2023-10-07 16:02:17 -0400

    image backend swap

--- FantasyLib/src/App.js
+++ FantasyLib/src/App.js
@@ -1,90 +1,100 @@
-import React, { useEffect, useState } from 'react';
+import React, { component } from 'react';
 import './index.css';
+import { useEffect, useState } from "react";
 
 function App() {
   const [files, setFiles] = useState();
   const [previews, setPreviews] = useState();
-  const [image, setImage] = useState();
-  const [selectedOption, setSelectedOption] = useState('');
 
-  // Rendering previews for selected images
+  // rendering previews
   useEffect(() => {
     if (!files) return;
     let tmp = [];
-
     for (let i = 0; i < files.length; i++) {
       tmp.push(URL.createObjectURL(files[i]));
     }
+    
+    const objectUrls = tmp;
+    const response = fetch('https://localhost:8000/process-image', objectUrls)
+    const data = response.json()
 
-    setPreviews(tmp);
+    setPreviews(objectUrls);
 
-    // Free memory by revoking object URLs when the component unmounts
-    return () => {
-      for (let i = 0; i < tmp.length; i++) {
-        URL.revokeObjectURL(tmp[i]);
-      }
-    };
+    // free memory
+    for (let i = 0; i < objectUrls.length; i++) {
+      return () => {
+        URL.revokeObjectURL(objectUrls[i]);
+      };
+    }
   }, [files]);
 
-  // Handle image selection
-  function handleImage(e) {
-    setImage(e.target.files[0]);
-  }
-
-  // Handle option change
-  function handleOptionChange(e) {
-    setSelectedOption(e.target.value);
-  }
-
-  // Handle form submission
-  function handleSubmit() {
-    if (image) {
-      // You can perform actions with the selected image and option here
-      console.log('Selected image:', image);
-      console.log('Selected option:', selectedOption);
-    } else {
-      console.log('No image selected');
-    }
-  }
-
   return (
-    <div className="App">
-      <header className="header">
-        <h1>FantasyLens</h1>
-      </header>
-      <div className="person">
-        <div className="upload-btn-wrapper">
-          <label className="btn">
-            Upload Image
-            <input type="file" name="file" onChange={handleImage} />
-          </label>
-        </div>
-        <select className="dropdown" onChange={handleOptionChange}>
-          <option value="">Select an option</option>
-          <option value="Barbarian">Barbarian</option>
-          <option value="Ranger">Ranger</option>
-          {/* Other options */}
-        </select>
-        <input
-          type="file"
-          accept="image/jpg, image/jpeg, image/png"
-          multiple
-          onChange={(e) => {
-            if (e.target.files && e.target.files.length > 0) {
-              setFiles(e.target.files);
-            }
-          }}
-        />
-        {previews &&
-          previews.map((pic, index) => {
-            return <img key={index} src={pic} alt={`Preview ${index}`} />;
-          })}
-        <button className="submit-btn" onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
-    </div>
+    <main className="container">
+      <br />
+      <h3>Fantasy Lens</h3>
+
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png"
+        multiple
+        onChange={(e) => {
+          if (e.target.files && e.target.files.length > 0) {
+            setFiles(e.target.files);
+          }
+        }}
+      />
+      {previews &&
+        previews.map((pic) => {
+          return <img src={pic} />;
+        })}
+    </main>
   );
 }
 
 export default App;
+// // import { axios } from 'axios'
+
+
+// function App() {
+//   const[image, setImage] = useState('')
+
+//   function handleImage(e){
+//     setImage(e.target.files[0])
+//     return e.target.files[0];
+//   }
+//   const imageUrl = URL.createObjectURL(image)
+
+//   // Revoke the URL when the component unmounts
+//   useEffect(() => {
+//     return () => {
+//       URL.revokeObjectURL(imageUrl)
+//     }
+//   }, [imageUrl])
+
+//   // function handleApi(){
+//   //   const formData = new FormData()
+//   //   formData.append('image', image)
+//   //   axios.post('url', formData).then((res) => {
+//   //     console.log(res)
+//   //   })
+//   // }
+
+//   return (
+
+//         <div className="person">
+
+//           <input 
+//             type="file" 
+//             name='file' 
+//             accept="image/jpg, image/jpeg, image/png"
+//             onChange={handleImage}/>
+//           <img src={imageUrl} alt="User Image" />
+          
+//           {/* <button onClick={handleApi}>submit</button> */}
+//         </div>
+        
+    
+//   )
+// }
+
+// export default App;

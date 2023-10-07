09a7a030071d755cb1ce6d625e82086c1117e6cb
runo-ev <runodotdash@gmail.com>
2023-10-07 12:20:00 -0400

    styling

--- FantasyLib/src/App.js
+++ FantasyLib/src/App.js
@@ -1,24 +1,31 @@
-import React, { component } from 'react';
-import {useState} from 'react'
-import './index.css';
-
+import React, { useState } from 'react';
+import './index.css'; // Import your CSS file
 
 function App() {
-  const[image, setImage] = useState('')
-  function handleImage(e){
-    console.log(e.targer.files[0])
-    setImage(e.target.files[0])
+  const [image, setImage] = useState(null);
+
+  function handleImage(e) {
+    setImage(e.target.files[0]);
+  }
+
+  function handleSubmit() {
+    if (image) {
+      // You can perform actions with the selected image here
+      console.log('Selected image:', image);
+    } else {
+      console.log('No image selected');
+    }
   }
 
   return (
-
-        <div className="person">
-
-          <input type="file" name='file' onChange={handleImage}/>
-          <button>submit</button>
-        </div>
-    
-  )
+    <div className="person">
+      <label>
+        Upload Image
+        <input type="file" name="file" onChange={handleImage} />
+      </label>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
 export default App;
